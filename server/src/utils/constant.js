e1c828d1e655015f9e75c7a28c9ad25ce0254b98
Aryal <roozanaryal8@gmail.com>
2025-08-14 07:44:33 +0545

    project setup

---
+++ server/src/utils/constant.js
@@ -0,0 +1,9 @@
+export const PORT = 5000;
+
+export const MONGODB_URI = 'mongodb://localhost:27017/';
+
+export const JWT_SECRET = 'your_jwt_secret';
+
+export const FRONTEND_URL = 'http://localhost:3000';
+
+export const BACKEND_URL = 'http://localhost:5000';
